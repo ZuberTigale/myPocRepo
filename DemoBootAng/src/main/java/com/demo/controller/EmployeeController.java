1b23c344d6dae5d4bf5bd6cd4474ba6d7006285a
ocean <ocean@DESKTOP-1NMU7VN>
2020-11-04 12:51:38 +0530

    cross origin updated

--- DemoBootAng/src/main/java/com/demo/controller/EmployeeController.java
+++ DemoBootAng/src/main/java/com/demo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.demo.model.Employee;
 import com.demo.repo.EmpRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/emp/v1")
 public class EmployeeController {
 	@Autowired
